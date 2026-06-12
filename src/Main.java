fd3df6749ad049fd7fbe5021e637053d86d74c39
LeaGuerin <leaguerin368@gmail.com>
2026-06-12 10:07:05 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import flou.FlouParMoyenne;
 
 public class Main {
+  public static void main(String[] args) {
+  
+    FlouParMoyenne flouMoy = new FlouParMoyenne();
+    flouMoy.setFlou("./img/originale.png", "./img/originaleFlouParMoyenne.png", 3);
+    
+  }
 
-  FlouParMoyenne flouMoy = new FlouParMoyenne();
-  flouMoy.setFlou("./img/originale.png", "./img/originaleFlouParMoyenne.png", 3);
 
 
 
