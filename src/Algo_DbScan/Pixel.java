3bc7bfe31d9030b1c89f32ad32b24b2333fddd87
Nathan_DUMONT <nathan.dumont9@etu.univ-lorraine.fr>
2026-06-17 12:52:33 +0200

    Ajout Pixel

---
+++ src/Algo_DbScan/Pixel.java
@@ -0,0 +1,13 @@
+package Algo_DbScan;
+
+public class Pixel {
+    int x ;
+    int y ;
+    int c ;
+
+    public Pixel (int x, int y, int couleur) {
+        this.x = x;
+        this.y = y;
+        this.c =couleur;
+    }
+}
